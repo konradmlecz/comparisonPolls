eeeeca44680f2581fd3783b8e5a69506ccbfe393
Konrad <konradmleczko85@gmail.com>
2019-08-07 19:42:24 +0200

    Updates

---
+++ precache-manifest.4fcd23645e9e7d17fc93f53a3b492221.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2765db2dd592d8aabcd105b9bcef2bdc",
+    "url": "/comparisonPolls/index.html"
+  },
+  {
+    "revision": "3897384e663386e69477",
+    "url": "/comparisonPolls/static/css/main.43655fba.chunk.css"
+  },
+  {
+    "revision": "f15348ae4496c4a38183",
+    "url": "/comparisonPolls/static/js/2.b7a0368d.chunk.js"
+  },
+  {
+    "revision": "3897384e663386e69477",
+    "url": "/comparisonPolls/static/js/main.6e92410c.chunk.js"
+  },
+  {
+    "revision": "9d0e46a177314d01ad1e",
+    "url": "/comparisonPolls/static/js/runtime~main.1de1f099.js"
+  },
+  {
+    "revision": "2d33317606b55faf2a54487525da22dd",
+    "url": "/comparisonPolls/static/media/ballot-box-32384.2d333176.svg"
+  },
+  {
+    "revision": "9d4d9079de0ac0b53bbb10783add1b10",
+    "url": "/comparisonPolls/static/media/poland-polish-5611.9d4d9079.jpg"
+  },
+  {
+    "revision": "315e4f2f06c6cf3e3bc984b47ce7a5c1",
+    "url": "/comparisonPolls/static/media/poland.315e4f2f.jpg"
+  }
+]);
